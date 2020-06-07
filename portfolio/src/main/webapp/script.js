6756da7021cb22817b14496cfb0287ec3ab6fbf9
gccurtis <gccurtis@google.com>
2020-06-07 07:41:06 +0000

    fixed problems from older branch

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,20 +35,9 @@ function removeChildren(node){
   while(node.hasChildNodes()){
     node.removeChild(children[0]);
   }
-  console.log(node.childNodes.length);
 }
 
 function deleteComment(id){
-  //const myHeaders = new Headers();
-  //myHeaders.append('Content-Type','text/plain');
-  //const formData = new FormData();
-  //formData.append("id2",id);
-  //formData.append("id3",id);
-  //console.log("id: "+id+" "+formData.get("id2"));
-  //var request = new XMLHttpRequest();
-  //request.open("POST", "/delete-comment");
-  //request.send(formData);
-  //fetch('/delete-comment',{method: 'POST',headers: myHeaders,body: new URLSearchParams("id="+String(id))});
   var oReq = new XMLHttpRequest();
   oReq.open("POST", "/delete-comment");
   oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -61,16 +50,12 @@ function loadComments(){
   const commentSection = document.getElementById('comment-section');
   removeChildren(commentSection);
   fetch('/comments?numberOfComments='+numberOfComments).then(response => response.json()).then((comments) => {
-    console.log(comments);
     for(i=0;i<comments.length;i++){
       const commentContainer = document.createElement("li");
       const commentId = comments[i].id;
       commentContainer.innerText = comments[i].message;
-      console.log(comments[i]);
-      console.log(comments[i].timestamp);
       commentContainer.setAttribute("id",comments[i].id);
       commentContainer.setAttribute("onClick","deleteComment(this.id)");
-      //commentContainer.onclick = (() => deleteComment(commentId));
       commentSection.appendChild(commentContainer);
     }});
 }
